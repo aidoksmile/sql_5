aa83f7b4947a4bb0741ac3dc79cab8942dcd8f11
Aida Shakharova <109811676+aidoksmile@users.noreply.github.com>
2023-04-02 11:34:52 +0600

    Урок 5. SQL – оконные функции

---
+++ sem5_hw.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE IF NOT EXISTS less5;
+USE less5;
+CREATE TABLE IF NOT EXISTS cars
+(
+	id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(45),
+    cost INT
+);
+
+INSERT cars
+VALUES
+	(1, "Audi", 52642),
+    (2, "Mercedes", 57127 ),
+    (3, "Skoda", 9000 ),
+    (4, "Volvo", 29000),
+	(5, "Bentley", 350000),
+    (6, "Citroen ", 21000 ), 
+    (7, "Hummer", 41400), 
+    (8, "Volkswagen ", 21600);
+    
+SELECT *
+FROM cars;
+
+-- 1.	Создайте представление, в которое попадут автомобили стоимостью  до 25 000 долларов
+CREATE VIEW budget_cars
+AS SELECT *
+FROM cars
+WHERE cost < 25000;
+
+-- 2.	Изменить в существующем представлении порог для стоимости: пусть цена будет до 30 000 долларов (используя оператор ALTER VIEW) 
+ALTER VIEW budget_cars
+AS SELECT *
+FROM cars
+WHERE cost < 30000;
+
+-- 3. 	Создайте представление, в котором будут только автомобили марки “Шкода” и “Ауди”
+CREATE VIEW skoda_audi
+AS SELECT *
+FROM cars
+WHERE name = "Skoda" OR name = "Audi";
+
+-- Добавьте новый столбец под названием «время до следующей станции». 
+-- Чтобы получить это значение, мы вычитаем время станций для пар смежных станций. 
+-- Мы можем вычислить это значение без использования оконной функции SQL, но это может быть очень сложно. 
+-- Проще это сделать с помощью оконной функции LEAD . 
+-- Эта функция сравнивает значения из одной строки со следующей строкой, чтобы получить результат. 
+-- В этом случае функция сравнивает значения в столбце «время» для станции со станцией сразу после нее.
+
+SELECT train_id, station, station_time,
+SUBTIME(LEAD(station_time) OVER(PARTITION BY train_id ORDER BY train_id), station_time)
+AS time_to_next_station
+FROM Trains;
